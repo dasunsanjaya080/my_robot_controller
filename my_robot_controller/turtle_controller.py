50831bddb9e6a7b5e6d87c7f61df260ca0c8900c
Dasun Sanjaya <dasunsanjaya080@gmail.com>
2026-01-06 11:26:28 +0530

    Update logic in turtle_controller node

--- my_robot_controller/turtle_controller.py
+++ my_robot_controller/turtle_controller.py
@@ -3,10 +3,13 @@ import rclpy #type: ignore
 from rclpy.node import Node #type: ignore
 from turtlesim.msg import Pose #type: ignore
 from geometry_msgs.msg import Twist #type: ignore
+from turtlesim.srv import SetPen #type: ignore
+# from functools import partial
 
 class TurtleController(Node):
     def __init__(self):
         super().__init__('turtle_controller')
+        self.previous_x = 0
         self.cmd_vel_publisher = self.create_publisher(Twist, 'turtle1/cmd_vel', 10)
         self.pose_subscriber = self.create_subscription(Pose, 'turtle1/pose', self.pose_callback, 10)
         self.get_logger().info("Turtle controller has been started")
@@ -21,7 +24,37 @@ class TurtleController(Node):
             cmd.angular.z = 0.0 
 
         self.cmd_vel_publisher.publish(cmd)
+
+        if pose.x > 5.5 and self.previous_x<= 5.5:
+            self.previous_x = pose.x
+            self.get_logger().info("Set color to red")
+            self.call_set_pen_service(255,0,0,3,0)
+        elif pose.x <=5.5 and self.previous_x>5.5:
+            self.previous_x = pose.x
+            self.get_logger().info("Set color to green")
+            self.call_set_pen_service(0,255,0,3,0)
         
+    def call_set_pen_service(self,r,g,b,width,off):
+        client = self.create_client(SetPen, "/turtle1/set_pen")
+        while not client.wait_for_service(1.0):
+            self.get_logger().warn("Waiting for Service")
+
+        request = SetPen.Request()
+        request.r = r
+        request.g = g
+        request.b = b
+        request.width = width
+        request.off = off
+
+        future = client.call_async(request)
+        future.add_done_callback(self.callback_set_pen)
+
+
+    def callback_set_pen(self,future):
+        try:
+            respone = future.result()
+        except Exception as e:
+            self.get_logger().error(f'Service call faild {e}')
 
 def main(args=None):
     rclpy.init(args=args)
